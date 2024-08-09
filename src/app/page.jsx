18e88bc21677c2c042e41c5e0a45e0865d9ab0de
Reece Young <reecejy00@gmail.com>
2024-08-09 10:03:16 +0000

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col container mx-auto p-4">
+      Homepage
+      <Hero />
+    </main>
+  );
+}
