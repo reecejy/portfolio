c5d569078c9e61c0fcb36f1c7c4dd9f5439a3995
Reece Young <reecejy00@gmail.com>
2024-08-09 11:06:06 +0000

    clean up

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className="flex items-center justify-center min-h-screen">
-      <div className="text-center max-w-xl uppercase">
-        <h1 className="text-6xl font-bold mb-4">Reece Young</h1>
-        <p className="text-lg mb-8 font-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam facilis quisquam, quod architecto maxime necessitatibus dolor fugit consectetur nostrum.</p>
+      <div className="flex flex-col items-center text-center uppercase max-w-[500px]">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Reece Young</h1>
+        <p className="text-base font-light mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam facilis quisquam, quod architecto maxime necessitatibus dolor fugit consectetur nostrum.</p>
       </div>
     </section>
   )
